ece552c22211ce293192532a63b014429c61f04b
Young1 <dlduddnjs198@naver.com>
2025-03-13 23:12:17 +0900

    [BOJ] 1000 A+B(bw연습)

---
+++ 백준/Solution1000.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+//A+B
+public class Main{
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        bw.write(String.valueOf(A+B));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
