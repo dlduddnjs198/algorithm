979017cdecbdd06bc74f192e73d56132dabfae6c
Young1 <dlduddnjs198@naver.com>
2025-03-17 05:02:23 +0900

    [BOJ] 1330 두 수 비교하기(bw연습)

---
+++ 백준/Solution1330.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+// 두 수 비교하기
+public class Main{
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        if(A<B) System.out.println("<");
+        else if(A>B) System.out.println(">");
+        else System.out.println("==");
+    }
+}
