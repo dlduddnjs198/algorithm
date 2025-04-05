7ef00f6857aeb42b48b04fc7fc17fe582c9849f1
Young1 <dlduddnjs198@naver.com>
2025-04-05 17:17:36 +0900

    [BOJ] 2557 Hello World

---
+++ 백준/Solution2557.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+// Hello World
+public class Main{
+
+    public static void main(String[] args) throws Exception{
+        System.out.println("Hello World!");
+    }
+}
