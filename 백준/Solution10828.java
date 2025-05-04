8862e9680e927006fe769255c5deeaed42aeef0f
Young1 <dlduddnjs198@naver.com>
2025-05-05 02:43:17 +0900

    [BOJ] 10828 스택

---
+++ 백준/Solution10828.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+// 스택
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        Deque<Integer> dq = new ArrayDeque<>();
+
+        for(int i=0;i<N;i++){
+            st = new StringTokenizer(br.readLine());
+            String cmd = st.nextToken();
+            switch(cmd){
+                case "push":
+                    dq.offerFirst(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop":
+                    if(dq.isEmpty()) sb.append("-1\n");
+                    else sb.append(dq.pollFirst()).append("\n");
+                    break;
+                case "size":
+                    sb.append(dq.size()).append("\n");
+                    break;
+                case "empty":
+                    if(dq.isEmpty()) sb.append("1\n");
+                    else sb.append("0\n");
+                    break;
+                case "top":
+                    if(dq.isEmpty()) sb.append("-1\n");
+                    else sb.append(dq.peekFirst()).append("\n");
+            }
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
