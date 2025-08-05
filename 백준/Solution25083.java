67d9ee22ba3e2e1c315dd03193e5f6bb1845ef1c
Young1 <dlduddnjs198@naver.com>
2025-08-05 12:58:47 +0900

    백준 25083 새싹

---
+++ 백준/Solution25083.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+// 25083 새싹
+public class Main{
+    public static void main(String[] args) throws Exception{
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
